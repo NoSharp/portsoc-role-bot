a9ba79bd8f5937e1968cdfe47d177b4d7290b856
nosharp <contact@nosharp.cc>
2023-01-29 13:30:40 +0000

    feat: add guild and client id config arguments

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -3,3 +3,11 @@ import { Envuments } from "envuments";
 export function getBotToken() : string{
   return Envuments.get("BOT_TOKEN");
 }
+
+export function getGuildId() : string {
+  return Envuments.get("GUILD_ID");
+}
+
+export function getClientId() : string {
+  return Envuments.get("BOT_CLIENT_ID");
+}
